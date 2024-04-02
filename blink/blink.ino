219e4896b059c5558cd4852ca6a8cc3f7996823e
Arda Karamoçu <140858696+Arda-Karamocu@users.noreply.github.com>
2024-04-02 11:46:34 +0300

    blink

---
+++ blink/blink.ino
@@ -0,0 +1,17 @@
+//Blink with built in led
+
+void setup() {
+  
+  pinMode(LED_BUILTIN,OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(500);
+
+
+}
